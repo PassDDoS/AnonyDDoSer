7477468eef92ff795f6e0e31bebc8f7ccb4f93b9
PassDDoS <41880206+PassDDoS@users.noreply.github.com>
2020-03-17 11:09:31 +0700

    Update anonyv28.py

--- anonyv28.py
+++ anonyv28.py
@@ -26,8 +26,6 @@ from random import randint
 urllib3.disable_warnings()
 urllib3.PoolManager()
 
-key_top = "VIP"
-
 useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
 "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
 "Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
